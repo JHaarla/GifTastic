34e2b6e80ba25dd05d794cd61f54f90d034c0932
JHaarla <haarladesign@gmail.com>
2019-08-20 23:49:05 -0400

    Big commit. most of the JS is done. Still need to animate/stop on click.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 // SETUP VARIABLES
 //==========================================
 var authKey = "N28xiqV40WuGfKE0cSITDuibMbKogzMo";
-var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + authKey + "&limit=10&offset=0&rating=PG-13" + "&q=" + userAddTerm;
+var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + authKey + "&limit=10&offset=0&rating=PG-13&q=";
 
 var stillURL = "";
-var animURL = "";
+var animatedURL = "";
 
 var isStill = true;
 
 // array of initial values/emotions to be displayed
-var emotions = ["tired", "confused", "mind blown", "hungry", "frustrated", "relaxed", "sassy", "suspicious"];
+var emotions = ["tired", "confused", "mind blown", "hungry", "hangry", "frustrated", "relaxed", "sassy", "suspicious"];
 var addedEmotions = [];
 
 // user input to add to buttons/array
@@ -29,8 +29,11 @@ function displayButtons() {
     for (var j = 0; j < addedEmotions.length; j++) {
         //create button for each array item
         var btnVal = addedEmotions[j];
+        // remove any spaces (not leading or trailing - those were already trimmed) from the user input. If this is not done, the API only returns values for the first term before the space
+        var queryVal = btnVal.replace(/\s+/g, "");
+
         // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
-        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", btnVal);
+        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
 
         $("#buttonsDiv").append(newBtn);
         // console.log(btnVal);
@@ -40,9 +43,18 @@ function displayButtons() {
 
 
 // AJAX call function & display GIFs
-function getGIFs(){
-    
-}
+// function getGIFs(){
+    // var queryURL = apiURLBase + userAddTerm;
+    // console.log(queryURL);
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(dataReturn) {
+    //         console.log(dataReturn);
+            
+    //     });
+
+// }
 
 //empty the buttons div and rewite the buttons to the Dom with the user added button
 function clear() {
@@ -50,8 +62,9 @@ function clear() {
     for (var i = 0; i < emotions.length; i++) {
         //create button for each array item
         var btnVal = emotions[i];
+        var queryVal = btnVal.replace(/\s+/g, "");
         // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
-        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", btnVal);
+        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
 
         $("#buttonsDiv").append(newBtn);
         // console.log(btnVal);
@@ -67,24 +80,77 @@ clear();
 
 // on.click event for the buttons up top - call the Giphy API and return 10 GIFs to the DOM
 
-$(this).on("click", function(){
+$("#buttonsDiv").on("click", ".my-emotion", function(){ //event delegation syntax for the on.click event to read dynamically added buttons
+console.log($(this).val());
+// getGIFs();
 
+var queryTerm = $(this).val();
+var queryURL = apiURLBase + queryTerm;
+console.log(queryURL); 
+// the ajax call and promise
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(dataReturn) {
+    console.log(dataReturn);
+    //this stores the data (the bit we're interested in) in a variable
+    var results = dataReturn.data;
+    //     
+    console.log(results);
+    for (var k = 0; k < results.length; k++) {
+        stillURL = results[k].images.fixed_height_still.url;
+        animatedURL = results[k].images.fixed_height.url;
+        var rating = results[k].rating;
+    console.log(stillURL);
+    console.log(animatedURL);
+
+        var GIFdiv = $("<div>");
+        var paraEl = $("<p>").text("Rating: " + rating).addClass("rating");
+        var emotionGIF = $("<img>");
+        emotionGIF.attr("src", stillURL).attr("gif-still", stillURL).attr("gif-animated", animatedURL).attr("gif-state", "still").addClass("gif");
+
+        GIFdiv.prepend(paraEl);
+        GIFdiv.append(emotionGIF);
+
+        $("#GifDiv").append(GIFdiv);
+
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+    })
 
 })
 
 
 // on.click event that adds another button
-$("#userInputBtn").on("click", function() {
-    // this should block an empty button to be displayed if the user clicks it without adding any text
-    //not working for some reason - still allows empty buttons to be added....boooo
-    // event.preventDefault();
+$("#userInputBtn").on("click", function(event) {
+// $(document).on("click", "#userInputBtn", function(event) {
 
+    // Blocks an empty button to be displayed if the user clicks it without adding any text
+        //Doesn't work for some reason...boooo
+    event.preventDefault();
+    //Grab user input & trim it
     userAddTerm = $("#addInput").val().trim();
     // push the user added input to addedEmotions array
+        // if conditional is to prevent and empty button since event.preventDefault(); didn't work...
+    if (userAddTerm !== "") {
     addedEmotions.push(userAddTerm);
     console.log(userAddTerm);
-// trigger the displayButtons functions to write all buttons to DOM
+    // reset input field to nothing so it can't be clicked repeadetly adding the last item over and over
+    $("#addInput").val("");
+    // trigger the displayButtons functions to write all buttons to DOM
     displayButtons();
+    }
 })
 
 
